39edffcda37be945667dc79882a245d507d8aee5
Aachman-Yadav <aachmanyadav22467@gmail.com>
2025-10-03 00:44:19 +0530

    Basic while loops

---
+++ while.js
@@ -0,0 +1,22 @@
+let index = 0;
+while (index <= 10) {
+    // console.log("YES");
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0;
+while( arr < myArray.length){
+    console.log(`Value of element of array is ${myArray[arr]}`);
+    arr++;
+}
+
+//* Kaam k baad condition hogi toh do while loop.....
+let score = 11;
+do {
+    console.log(`The score is ${score}`);
+    score++;
+    
+} while (score <= 10);
